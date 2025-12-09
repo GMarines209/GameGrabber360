e0893e611c92845b206f5f587a5e2f488d010a20
GMarines209 <gabrielmarines209@gmail.com>
2025-12-09 16:47:56 -0500

    futher devloped search function

--- main.c
+++ main.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
+#include <dirent.h>
+#include <ctype.h>
+
+//  C:\Users\Gabriel\Downloads\games.txt
+
+void toLowerString(char* str){
+    for(int i = 0; str[i]; i++){
+        str[i] = tolower(str[i]);
+    }
+}
+
+int search_repo(char* game_name){
+    
+    //init game repo directory
+    DIR *game_repo;
+    struct dirent *entry;
+
+    //make case insensitive and remove new line
+    toLowerString(game_name);
+    game_name[strcspn(game_name, "\n")] = 0;
+
+    game_repo = opendir("E:\\");
+    if (game_repo == NULL) {
+        printf("Could not open E drive.\n");
+        return 1;
+    }
+    
+    while ((entry = readdir(game_repo)) != NULL)
+    {
+        if (strstr(entry->d_name, game_name) != NULL)
+        {
+            printf("Found match: %s\n", entry->d_name);
+        }
+        
+    }
+    
+
+
+}
 
 int main() {
 
@@ -8,14 +47,7 @@ int main() {
 
     char client_list_path[256];
     fgets(client_list_path, 256, stdin);
-
     client_list_path[strcspn(client_list_path, "\n")] = 0; 
-
-    printf("Where are the games stored?");
-    char games_path[256];
-    fgets(games_path, 256, stdin);
-    games_path[strcspn(games_path, "\n")] = 0; 
-
     FILE *client_list_file = fopen(client_list_path, "r");
 
     if (client_list_file == NULL) {
@@ -25,9 +57,11 @@ int main() {
 
         //process line by line
         char line_buffer[256]; 
+        line_buffer[strcspn(line_buffer, "\n")] = 0;
+
 
         while (fgets(line_buffer, 256, client_list_file) != NULL) {
-            
+            search_repo(line_buffer);
         }
         
         fclose(client_list_file); 
@@ -35,3 +69,6 @@ int main() {
 
     return 0;
 }
+
+
+

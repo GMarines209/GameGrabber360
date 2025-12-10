073d87fc4e41971e0b09c6b2462c37c016c6c4ed
GMarines209 <gabrielmarines209@gmail.com>
2025-12-10 13:45:16 -0500

    improved file parsing

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 #include <string.h>
 #include <dirent.h>
 #include <ctype.h>
+#include <math.h>
+
+int Lev_Distance(char* str1, char* str2){
+    
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+
+
+}
 
 //  C:\Users\Gabriel\Downloads\games.txt
 
@@ -27,8 +36,10 @@ int search_repo(char* game_name){
         return 1;
     }
     
+    // loop through game repo and find file name matches
     while ((entry = readdir(game_repo)) != NULL)
     {
+        toLowerString(entry->d_name);
         if (strstr(entry->d_name, game_name) != NULL)
         {
             printf("Found match: %s\n", entry->d_name);
@@ -43,11 +54,12 @@ int search_repo(char* game_name){
 int main() {
 
     // 1. Take in requested games list
-    printf("What is the filepath to the client text file? ");
+    printf("What is the filepath to the client text file? "); 
 
     char client_list_path[256];
     fgets(client_list_path, 256, stdin);
     client_list_path[strcspn(client_list_path, "\n")] = 0; 
+    
     FILE *client_list_file = fopen(client_list_path, "r");
 
     if (client_list_file == NULL) {
